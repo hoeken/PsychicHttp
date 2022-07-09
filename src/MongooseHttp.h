b25de9742e63b8d149313958969b5b97795aa80f
Jeremy Poulter <jpoulter@webgains.com>
2022-07-09 19:57:09 +0100

    Make compatible with ESPAsyncWebServer (kind of)

--- src/MongooseHttp.h
+++ src/MongooseHttp.h
@@ -23,6 +23,7 @@
 #define ARDUINO_MONGOOSE_SEND_BUFFER_SIZE 256
 #endif
 
+#ifndef _ESPAsyncWebServer_H_
 typedef enum {
   HTTP_GET     = 0b00000001,
   HTTP_POST    = 0b00000010,
@@ -33,6 +34,9 @@ typedef enum {
   HTTP_OPTIONS = 0b01000000,
   HTTP_ANY     = 0b01111111,
 } HttpRequestMethod;
+#else
+typedef WebRequestMethod HttpRequestMethod;
+#endif
 
 typedef uint8_t HttpRequestMethodComposite;
 
