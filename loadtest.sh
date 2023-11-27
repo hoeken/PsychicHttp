90ea99c78c5d5808f23e868e0e70be138b73bdf1
Zach Hoeken <hoeken@gmail.com>
2023-11-27 13:32:53 -0500

    added single and multiple tests

--- loadtest.sh
+++ loadtest.sh
@@ -3,8 +3,6 @@
 # npm install -i loadtest
 
 TEST_IP="192.168.2.131"
-CONCURRENCY=1
-CORES=1
 TEST_TIME=60
 LOG_FILE=psychic-http-loadtest.log
 
@@ -12,16 +10,34 @@ if test -f "$LOG_FILE"; then
   rm $LOG_FILE
 fi
 
-echo "Testing http://$TEST_IP/"
+CONCURRENCY=1
+CORES=1
+
+echo "Testing http://$TEST_IP/ single client"
 loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout 5000 "http://$TEST_IP/" --quiet >> $LOG_FILE
 
-echo "Testing http://$TEST_IP/api"
+echo "Testing http://$TEST_IP/api single client"
 loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout 5000 "http://$TEST_IP/api?foo=bar" --quiet >> $LOG_FILE
 
-echo "Testing http://$TEST_IP/ws"
+echo "Testing http://$TEST_IP/ws single client"
 loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout 5000 "ws://$TEST_IP/ws" --quiet 2> /dev/null >> $LOG_FILE
 
-echo "Testing http://$TEST_IP/alien.png"
+echo "Testing http://$TEST_IP/alien.png single client"
+loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout 5000 "http://$TEST_IP/alien.png" --quiet >> $LOG_FILE
+
+CONCURRENCY=5
+CORES=1
+
+echo "Testing http://$TEST_IP/ multiple clients"
+loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout 5000 "http://$TEST_IP/" --quiet >> $LOG_FILE
+
+echo "Testing http://$TEST_IP/api multiple clients"
+loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout 5000 "http://$TEST_IP/api?foo=bar" --quiet >> $LOG_FILE
+
+echo "Testing http://$TEST_IP/ws multiple clients"
+loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout 5000 "ws://$TEST_IP/ws" --quiet 2> /dev/null >> $LOG_FILE
+
+echo "Testing http://$TEST_IP/alien.png multiple clients"
 loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout 5000 "http://$TEST_IP/alien.png" --quiet >> $LOG_FILE
 
 #some basic test commands:
