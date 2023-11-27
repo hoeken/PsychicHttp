c2b0a7a87b577c1cbd88f533cf6554049f4d43a1
Zach Hoeken <hoeken@gmail.com>
2023-11-27 14:49:41 -0500

    made timeout configurable

--- loadtest.sh
+++ loadtest.sh
@@ -5,6 +5,7 @@
 TEST_IP="192.168.2.131"
 TEST_TIME=60
 LOG_FILE=psychic-http-loadtest.log
+TIMEOUT=5000
 
 if test -f "$LOG_FILE"; then
   rm $LOG_FILE
@@ -14,31 +15,31 @@ CONCURRENCY=1
 CORES=1
 
 echo "Testing http://$TEST_IP/ single client"
-loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout 5000 "http://$TEST_IP/" --quiet >> $LOG_FILE
+loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout $TIMEOUT "http://$TEST_IP/" --quiet >> $LOG_FILE
 
 echo "Testing http://$TEST_IP/api single client"
-loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout 5000 "http://$TEST_IP/api?foo=bar" --quiet >> $LOG_FILE
+loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout $TIMEOUT "http://$TEST_IP/api?foo=bar" --quiet >> $LOG_FILE
 
 echo "Testing http://$TEST_IP/ws single client"
-loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout 5000 "ws://$TEST_IP/ws" --quiet 2> /dev/null >> $LOG_FILE
+loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout $TIMEOUT "ws://$TEST_IP/ws" --quiet 2> /dev/null >> $LOG_FILE
 
 echo "Testing http://$TEST_IP/alien.png single client"
-loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout 5000 "http://$TEST_IP/alien.png" --quiet >> $LOG_FILE
+loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout $TIMEOUT "http://$TEST_IP/alien.png" --quiet >> $LOG_FILE
 
 CONCURRENCY=5
 CORES=1
 
 echo "Testing http://$TEST_IP/ multiple clients"
-loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout 5000 "http://$TEST_IP/" --quiet >> $LOG_FILE
+loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout $TIMEOUT "http://$TEST_IP/" --quiet >> $LOG_FILE
 
 echo "Testing http://$TEST_IP/api multiple clients"
-loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout 5000 "http://$TEST_IP/api?foo=bar" --quiet >> $LOG_FILE
+loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout $TIMEOUT "http://$TEST_IP/api?foo=bar" --quiet >> $LOG_FILE
 
 echo "Testing http://$TEST_IP/ws multiple clients"
-loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout 5000 "ws://$TEST_IP/ws" --quiet 2> /dev/null >> $LOG_FILE
+loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout $TIMEOUT "ws://$TEST_IP/ws" --quiet 2> /dev/null >> $LOG_FILE
 
 echo "Testing http://$TEST_IP/alien.png multiple clients"
-loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout 5000 "http://$TEST_IP/alien.png" --quiet >> $LOG_FILE
+loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --timeout $TIMEOUT "http://$TEST_IP/alien.png" --quiet >> $LOG_FILE
 
 #some basic test commands:
 # curl http://psychic.local/
