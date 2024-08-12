990ae8dcf5631b1b4f1a8f88d32fa720e88af6ee
Zach Hoeken <hoeken@gmail.com>
2024-08-12 12:36:25 -0500

    test script setting clients properly now.

--- benchmark/loadtest-websocket.sh
+++ benchmark/loadtest-websocket.sh
@@ -15,20 +15,31 @@ fi
 
 echo "url,clients,rps,latency,errors" > $RESULTS_FILE
 
-for CONCURRENCY in 1 2 3 4 5 6 7 8 9 10 15 20
+CORES=1
+for CONCURRENCY in 1 2 3 4 5
 do
   echo "Testing $CONCURRENCY clients on $PROTOCOL://$TEST_IP/ws"
-  loadtest -c $CONCURRENCY --cores 1 -t $TEST_TIME --insecure $PROTOCOL://$TEST_IP/ws --quiet  2> /dev/null >> $LOG_FILE
+  loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --insecure $PROTOCOL://$TEST_IP/ws --quiet  2> /dev/null >> $LOG_FILE
   node parse-websocket-test.js $LOG_FILE $RESULTS_FILE
-  sleep 5
+  sleep 2
 done
 
-#for CONNECTIONS in 8 10 16 20
-#for CONNECTIONS in 20
-#do
-#  CONCURRENCY=$((CONNECTIONS / 2))
-#  echo "Testing $CONNECTIONS clients on $PROTOCOL://$TEST_IP/ws"
-#  loadtest -c $CONCURRENCY --cores 1 -t $TEST_TIME --insecure $PROTOCOL://$TEST_IP/ws --quiet 2> /dev/null >> $LOG_FILE
-#  node parse-websocket-test.js $LOG_FILE $RESULTS_FILE
-#  sleep 5
-#done
+CORES=2
+for CONNECTIONS in 6 8 10 12 14
+do
+  CONCURRENCY=$((CONNECTIONS / 2))
+  echo "Testing $CONNECTIONS clients on $PROTOCOL://$TEST_IP/ws"
+  loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --insecure $PROTOCOL://$TEST_IP/ws --quiet  2> /dev/null >> $LOG_FILE
+  node parse-websocket-test.js $LOG_FILE $RESULTS_FILE
+  sleep 2
+done
+
+CORES=4
+for CONNECTIONS in 16 20 24 28 32
+do
+ CONCURRENCY=$((CONNECTIONS / CORES))
+ echo "Testing $CONNECTIONS clients on $PROTOCOL://$TEST_IP/ws"
+ loadtest -c $CONCURRENCY --cores $CORES -t $TEST_TIME --insecure $PROTOCOL://$TEST_IP/ws --quiet 2> /dev/null >> $LOG_FILE
+ node parse-websocket-test.js $LOG_FILE $RESULTS_FILE
+ sleep 2
+done
