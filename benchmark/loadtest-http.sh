54f65ea3ab077b95a23af3385a4328940755a1db
Zach Hoeken <hoeken@gmail.com>
2024-08-12 11:51:36 -0500

    updates to benchmark script

--- benchmark/loadtest-http.sh
+++ benchmark/loadtest-http.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 #Command to install the testers:
-# npm install -g autocannon csv-writer
+# npm install
 
 TEST_IP="psychic.local"
-TEST_TIME=60
+TEST_TIME=10
 #LOG_FILE=psychic-http-loadtest.log
 LOG_FILE=_psychic-http-loadtest.json
 RESULTS_FILE=http-loadtest-results.csv
 TIMEOUT=10000
-WORKERS=1
+WORKERS=10
 PROTOCOL=http
 #PROTOCOL=https
 
@@ -20,6 +20,7 @@ do
   echo "Testing $CONCURRENCY clients on $PROTOCOL://$TEST_IP/"
   autocannon -c $CONCURRENCY -w $WORKERS -d $TEST_TIME -j "$PROTOCOL://$TEST_IP/" > $LOG_FILE
   node parse-http-test.js $LOG_FILE $RESULTS_FILE
+  sleep 5
 done
 
 for CONCURRENCY in 1 2 3 4 5 6 7 8 9 10 15 20
@@ -27,6 +28,7 @@ do
   echo "Testing $CONCURRENCY clients on $PROTOCOL://$TEST_IP/api"
   autocannon -c $CONCURRENCY -w $WORKERS -d $TEST_TIME -j "$PROTOCOL://$TEST_IP/api?foo=bar" > $LOG_FILE
   node parse-http-test.js $LOG_FILE $RESULTS_FILE
+  sleep 5
 done
 
 for CONCURRENCY in 1 2 3 4 5 6 7 8 9 10 15 20
@@ -34,6 +36,7 @@ do
   echo "Testing $CONCURRENCY clients on $PROTOCOL://$TEST_IP/alien.png"
   autocannon -c $CONCURRENCY -w $WORKERS -d $TEST_TIME -j "$PROTOCOL://$TEST_IP/alien.png" > $LOG_FILE
   node parse-http-test.js $LOG_FILE $RESULTS_FILE
+  sleep 5
 done
 
 rm $LOG_FILE
